5d044dbda198838e9099bbcf9c37e73b66b4c190
Sergey Surin <nastenka-rom@yandex.ru>
2020-10-06 17:12:09 +0300

    add tests for login page

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        url_text = "login"
+        assert url_text in self.browser.current_url, \
+            f'Text "{url_text}" is missing in current url'
+
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), \
+            "Login form is missing"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), \
+            "Register form is missing"
